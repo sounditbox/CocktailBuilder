f44ddcb5e8622593ac7ade0bb41a626a64c2f5d2
matthew <k.matvey.t@gmail.com>
2024-06-16 20:21:47 +0300

    added new cocktail

--- main.py
+++ main.py
@@ -15,3 +15,9 @@ white_russian = Cocktail('White Russian', [
     Ingredient('Prostokvashino Cream', 30)
 ])
 white_russian.info()
+
+B52 = Cocktail("B-52",[
+    Ingredient("Baileys", 20),
+    Ingredient("Cuantro", 20),
+    Ingredient("Kahlua", 20)
+]  )
